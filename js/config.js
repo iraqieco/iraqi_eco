c7b4f7b89e51df9bcf75a815885a3fe06caeaad3
iraqi_eco <0m7q7k@gmail.com>
2026-07-16 16:35:08 +0300

    Add Supabase configuration to config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://rmqtopxawrrgntcqqnpa.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtcXRvcHhhd3JyZ250Y3FxbnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3OTIxMzQsImV4cCI6MjA5ODM2ODEzNH0.HoExHY9oNFdryWUC2u2UELLPfBNhH3P4L3zS03dvELA";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
